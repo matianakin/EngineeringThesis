17dce5faf79a24a52bbb27f1991533636a92ec1c
mategra599 <mategra599@student.polsl.pl>
2022-12-12 01:22:42 +0100

    can get xhtml of a live page

--- Version 1/src/project/model/Model.java
+++ Version 1/src/project/model/Model.java
@@ -1,6 +1,7 @@
 package project.model;
 
 import java.io.*;
+import java.net.URL;
 
 public class Model {
 
@@ -28,7 +29,6 @@ public class Model {
         return ret;
     }
 
-
     public String readFromXtmhlFile(String path) {
         String ret = null;
         try {
@@ -37,5 +37,36 @@ public class Model {
             e.printStackTrace();
         }
         return ret;
+    }
+
+    public void printStringArray(String[] array) {
+        for (String s : array) {
+            System.out.println(s);
         }
+    }
+
+    public void GetXHTML (String adress) {
+        try {
+            // Create a URL object from the user-entered URL string
+            URL url = new URL(adress);
+
+            // Open a stream to the URL and read the XHTML into a StringBuilder
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuilder xhtml = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                xhtml.append(line);
+            }
+
+            // Print the XHTML
+            //System.out.println(xhtml);
+            PrintWriter writer = new PrintWriter("xhtml.txt", "UTF-8");
+            writer.println(xhtml);
+            writer.close();
+        } catch (Exception e) {
+            // Print an error message if the URL is invalid or there is a problem reading the XHTML
+            System.out.println("Error: Invalid URL or problem reading XHTML");
+        }
+    }
+
 }
